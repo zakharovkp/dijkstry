5d62a2e67b1dc89a13e07106789ba965acdaa50b
zakharovkp <vulcan.z@yandex.ru>
2024-05-18 15:01:03 +0300

    Update lab 2.py

--- lab 2.py
+++ lab 2.py
@@ -4,9 +4,9 @@ import networkx as nx  # импортируем библиотеки
 G = nx.Graph()         # создаем пустой граф
 G.add_nodes_from([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]) # добавляем вершины в граф
 G.add_edges_from([(1, 2), (2, 3), (3, 4), (4, 5), (5, 6), (6, 7), (7, 8), (8, 9), (9, 10)]) # добавляем ребра в граф
-centrality = nx.eigenvector_centrality_numpy(G)
+centrality = nx.eigenvector_centrality_numpy(G) # расчитываем меры центральности
 for n in centrality:
-    print("c(", n, ")=", centrality[n])
+    print("c(", n, ")=", centrality[n]) # выводим результаты расчета для каждого узла
 nx.draw(G, node_size=300, with_labels=True, node_color='y') # визуализация графа
 plt.savefig("g1.png") # сохранение графа как png файл
 plt.show() # вывод картинки на экран
